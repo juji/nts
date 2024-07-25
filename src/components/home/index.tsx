1423e59f5fa1afbfbbf64bcea8cbb06d5e891f61
juji <him@jujiyangasli.com>
2024-07-26 03:13:14 +0700

    use textarea for title

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,6 +1,43 @@
 import { onPaste } from "~/lib/clean-clipboard-data"
 import styles from './style.module.css'
 import { Milkdown } from "../milkdown"
+import UAParser from 'ua-parser-js'
+import { createSignal } from "solid-js"
+
+function TitleEditable({
+  initialContent,
+  onChange
+}:{
+  initialContent: string
+  onChange?: (str: string) => void
+}){
+
+  const [ content, setContent ] = createSignal(initialContent)
+  const splitContent = () => content().split('\n')
+
+  function onChangeLocal(e: InputEvent){
+    const target = e.target as HTMLTextAreaElement
+    setContent(target.value)
+    onChange && onChange(target.value) 
+  }
+
+  // https://codepen.io/shshaw/pen/bGNJJBE
+  // https://css-tricks.com/auto-growing-inputs-textareas/
+  return <div class={styles.textarea}>
+    {splitContent().map((v,i,a) => {
+      return <>
+        {v}{i === a.length-1 ? '' : <br />}
+      </>
+    })}
+    <textarea 
+      placeholder="Title"
+      value={content()}
+      onPaste={onPaste}
+      onInput={onChangeLocal}></textarea>
+  </div>
+
+}
+
 
 
 export default function Home(){
@@ -8,7 +45,7 @@ export default function Home(){
   return <div class={styles.container}>
 
     <div class={styles.headingContainer}>
-      <h1 contentEditable onPaste={onPaste}></h1>
+      <TitleEditable initialContent={""} />
       <p>
         <span>{new Date().toLocaleString()}</span>
         <span>&nbsp;&nbsp;︱&nbsp;&nbsp;</span>
